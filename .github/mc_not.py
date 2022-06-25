d754a4d137ae4f44a3d467e600c5ff5829ee84d2
Priyadharshini-Sakthivel <75614879+Priyadharshini-Sakthivel@users.noreply.github.com>
2022-06-25 23:37:37 +0530

    Update mc_not.py

--- .github/mc_not.py
+++ .github/mc_not.py
@@ -1,20 +1,4 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "[0 1] value 0\n",
-      "[1 1] value 0\n"
-     ]
-    }
-   ],
-   "source": [
+
     "#McCulloh Pitt model for AND model\n",
     "import numpy as np\n",
     "\n",
@@ -35,35 +19,4 @@
     "print (x0, \"value\", ans1)\n",
     "ans2 = McNot(x1,w,t)\n",
     "print (x1, \"value\", ans2)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+   
