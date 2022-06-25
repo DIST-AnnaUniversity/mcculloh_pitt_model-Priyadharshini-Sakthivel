35d05cb0a32f1c25c627e73030d5bf956a69ee9f
Priyadharshini-Sakthivel <75614879+Priyadharshini-Sakthivel@users.noreply.github.com>
2022-06-25 09:58:24 +0530

    Update mcpitt.py

--- .github/mcpitt.py
+++ .github/mcpitt.py
@@ -1,23 +1,4 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "id": "bf0ef67b",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "[0 0 1] value 0\n",
-      "[0 1 1] value 1\n",
-      "[1 0 1] value 1\n",
-      "[1 1 1] value 1\n"
-     ]
-    }
-   ],
-   "source": [
+
     "#McCulloh Pitt model for AND model\n",
     "import numpy as np\n",
     "\n",
@@ -44,36 +25,4 @@
     "print (x2, \"value\", ans3)\n",
     "ans4 = McOr(x3,w,t)\n",
     "print (x3, \"value\", ans4)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "61f76e86",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.12"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+ 
